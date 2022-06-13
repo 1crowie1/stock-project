5baedb71a3e12c438318535679e820eebfa1b9a8
Harrison Crowe-Maxwell <39574251+1crowie1@users.noreply.github.com>
2022-06-13 14:15:33 +1000

    Logging setup for insights, color coord for logs implemented

--- data-analysis/insights.py
+++ data-analysis/insights.py
@@ -1,10 +1,18 @@
 import dbapi
+import logging
 
 def main() -> None:
+    script_name = __file__.split('\\')[-1]
+    level = logging.DEBUG
+    fmt = f'[%(levelname)s] \033[33m{script_name}\033[0m %(asctime)s: %(message)s'
+    logging.basicConfig(level=level, format=fmt)
+
+    logging.info('\033[34m\033[4mApplication Started\033[0m')
+
     if not dbapi.check_conn():
-        print('\033[31m Database connection failed.')
+        logging.warning('\033[31mDatabase connection failed.\033[0m')
         return
-    print('\033[32mDatabase connection successful.')
+    logging.info('\033[32mDatabase connection successful.\033[0m')
 
 if __name__ == '__main__':
     main()
