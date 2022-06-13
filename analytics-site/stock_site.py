0489067c4d0a1b1b7c3851f6456e696b21cee264
Harrison Crowe-Maxwell <39574251+1crowie1@users.noreply.github.com>
2022-06-13 11:11:40 +1000

    Basic Site Data, heading and description

--- analytics-site/stock_site.py
+++ analytics-site/stock_site.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Hello, World!</h1>"
+    return "<div><h1>Stock Analytics</h1></div><div><h2>Displays data analysis and algorithm trading performance</h2></div>"
