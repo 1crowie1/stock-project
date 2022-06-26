d35be07f2be435646996e591ca75f9db02e39d14
Kyle Todd <ktodd087@gmail.com>
2022-06-26 13:45:22 +1000

    Tweet storage and Popular tweets
    Setup storage of tweets and filter for tweets with popularity

--- stock_scraper/twitter_scraper/main.py
+++ stock_scraper/twitter_scraper/main.py
@@ -1,11 +1,11 @@
 # Import unofficial twitter API
-
 import twint
 
 #Test configuration
 config = twint.Config()
 config.Search = "$APPL"
-config.Output = "tweets.csv"
+config.Output = "appl.csv"
+config.Popular_tweets = True
 
 #Turn saving into a csv on or off
 config.Store_csv = False
