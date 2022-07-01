1c3e167a123b23d78e146dbc8a2bee58bf617224
Kyle Todd <ktodd087@gmail.com>
2022-07-02 03:32:31 +1000

    Update scraper to store files to json

--- stock-scraper/scrape_twitter.py
+++ stock-scraper/scrape_twitter.py
@@ -3,11 +3,15 @@ import twint
 
 def config(ticker,store_data):
     config = twint.Config()
+    config.Custom["tweet"] = ["id","created_at","username","tweet","cashtags"]
+    config.Store_json = True
+    config.Store_object = True
+    config.Min_likes = 1000
     config.Search = ticker
-    config.Output = ticker + ".csv"
+    config.Output = "./"+ticker + ".json"
     config.Popular_tweets = True
-    config.Store_csv = False
+    config.Lang = "en"
     return config
 
 #Run scrape
-twint.run.Search(config(input("Ticker:" ),False))
+twint.run.Search(config(input("Search: " ),False))
