86f9c37e6dfba4530eb314cc283f93f73a818f93
Kyle Todd <ktodd087@gmail.com>
2022-07-08 16:28:13 +1000

    Change scrape to json object

--- stock-scraper/scrape_twitter.py
+++ stock-scraper/scrape_twitter.py
@@ -1,17 +1,31 @@
 # Import unofficial twitter API
 import twint
 
-def config(ticker,store_data):
-    config = twint.Config()
-    config.Custom["tweet"] = ["id","created_at","username","tweet","cashtags"]
-    config.Store_json = True
-    config.Store_object = True
-    config.Min_likes = 1000
-    config.Search = ticker
-    config.Output = "./"+ticker + ".json"
-    config.Popular_tweets = True
-    config.Lang = "en"
-    return config
+class twitter_scrape:
+    # def __init__(self, config):
+    #     self.config = config
+    
+    def config(ticker):
+        config = twint.Config()
+        config.Custom["tweet"] = ["id","created_at","username","tweet","cashtags"]
+        config.Store_json = True
+        config.Store_object = True
+        config.Min_likes = 1000
+        config.Search = ticker
+        config.Output = "./"+ticker + ".json"
+        config.Popular_tweets = True
+        config.Lang = "en"
 
-#Run scrape
-twint.run.Search(config(input("Search: " ),False))
+        #Display output to command line bool.
+        config.Hide_output = False
+        return config
+
+    def run_scrape(self, search):
+        twint.run.Search(twitter_scrape.config(search))
+
+def main():
+    scrape = twitter_scrape()
+    scrape.run_scrape(input('Search: '))
+
+main() 
+
