7c2ee20fe802c6f9c7199deddfba787f71b26f39
Harrison Crowe-Maxwell <39574251+1crowie1@users.noreply.github.com>
2022-07-09 17:39:12 +1000

    defining columns

--- data-pipeline/transform.py
+++ data-pipeline/transform.py
@@ -5,5 +5,5 @@ def format_reddit_data(subs_data: pd.DataFrame) -> pd.DataFrame:
     """
     Format data for loading into Azure Database
     """
-    
+    columns = ['sub_id', 'post_user', 'post_title', 'post_text', 'post_time']
     return
