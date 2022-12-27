50ed875a15c88687250b0729dc37ca5cd53d42ed
Kyle Todd <ktodd087@gmail.com>
2022-12-27 17:44:47 +1100

    Add second sentiment library
    Added second sentiment library for second opinion + subjectivity rating

--- data-pipeline/scrape_twitter.py
+++ data-pipeline/scrape_twitter.py
@@ -23,27 +23,40 @@ class Twitter:
 
         #Extract tweet and id to analyse sentiment
         tweets = twint_to_pd(["tweet", "id"])
-        sentiment_array = []
+        vader_sentiment_array = []
+        tb_sentiment_array = []
+        subjectivity_sentiment_array = []
 
         # Go through tweeet rows and check sentiment, print Tweet content,
         # and change sentiment number into categorical data. Add all sentiments into array
         for i, j in tweets.iterrows():
-            sen = sentiment.sentiment_analysis(j)
-            print(sentiment.sentiment_text(sen))
-            sentiment_array.append(sen)
+            sen = sentiment.vadersen.sentiment_analysis(j)
+            sen2p = sentiment.tbsen.sentiment_analysis_p(j[0])
+            sen2s = sentiment.tbsen.sentiment_analysis_s(j[0])
+            
+            # Adding the values to their arrays so they can be added to pd
+            vader_sentiment_array.append(sen)
+            tb_sentiment_array.append(sen2p)
+            subjectivity_sentiment_array.append(sen2s)
+
+            #Prints for testing 
+            print(sentiment.vadersen.sentiment_text(sen))
+            print(str(sen2p) + " - Polarity")
+            print(str(sen2s) + " - Subjectivity")
             print("Tweet: "+j[0])
             print("Twitter url: https://twitter.com/anyuser/status/"+j[1])
             print()
 
-        # Append the sentiment array to the original pandas dataframe and give an average
-        # this is mostly for testing. 
-        print("Average: " + str(sentiment.get_avg_sentiment()))
+        # Give an average from the vader array
+        print("Average: " + str(sentiment.vadersen.get_avg_sentiment()))
 
-        data["sentiment"] = sentiment_array # Has setting with copy error that needs to be fixed 
-        
+        # Append the sentiment arrays to the original pandas dataframe
+        data["sentiment1"] = vader_sentiment_array # Has setting with copy error that needs to be fixed 
+        data["sentiment2"] = tb_sentiment_array 
+        data["subjectivity"] = subjectivity_sentiment_array
 
-        # Return the new dataframe with tweet sentiment in a new column
+        # Return the new dataframe with tweet sentiment in a new columns
         return data
 
 #Test scrape term 
-print(Twitter.scrape("MSFT"))
+print(Twitter.scrape("TSLA"))
--- data-pipeline/sentiment.py
+++ data-pipeline/sentiment.py
@@ -1,29 +1,43 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as sentiment
-
-# Sentiment analysis function
-def sentiment_analysis(data): 
-    for sentence in data:
-        vs = sentiment().polarity_scores(sentence)
-        return(vs['compound'])
+from textblob import TextBlob
 
 avg_sentiment_array = []
 
 def Average(lst):
     return sum(lst) / len(lst)
+    
+class tbsen:
+    # this could probably be simplified into one function ...
+    #Textblob returns two values p - polaity & s - subjectivity
+    def sentiment_analysis_p(data):
+        tbsentiment = TextBlob(data)
+        return tbsentiment.sentiment[0]
+        # Returns polarity -1 = Negative & +1 = Positive
+    
+    def sentiment_analysis_s(data):
+        tbsentiment = TextBlob(data)
+        return tbsentiment.sentiment[1]
+        # Returns objectivity 0 = Very Objective & 1 = Very Subjective
+class vadersen:
+    # Sentiment analysis function
+    def sentiment_analysis(data): 
+        for sentence in data:
+            vadersentiment = sentiment().polarity_scores(sentence)
+            return(vadersentiment['compound'])
 
-def get_avg_sentiment():
-    return Average(avg_sentiment_array)
+    def get_avg_sentiment():
+        return Average(avg_sentiment_array)
 
-# Add categorical sentiment to the returned value
-def sentiment_text(sen):
-    if sen >= 0.05:
-        avg_sentiment_array.append(sen)
-        return str(sen) + " - Positive"
-    elif sen <= -0.05:
-        avg_sentiment_array.append(sen)
-        return str(sen) + " - Negative"
-    elif sen > -0.05 or sen <0.05:
-        avg_sentiment_array.append(sen)
-        return str(sen) + " - Neutral"
+    # Add categorical sentiment to the returned value for testing
+    def sentiment_text(sen):
+        if sen >= 0.05:
+            avg_sentiment_array.append(sen)
+            return str(sen) + " - Positive"
+        elif sen <= -0.05:
+            avg_sentiment_array.append(sen)
+            return str(sen) + " - Negative"
+        elif sen > -0.05 or sen <0.05:
+            avg_sentiment_array.append(sen)
+            return str(sen) + " - Neutral"
 
 
